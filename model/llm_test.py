6ab0e139d60bfb310e0c854b712ae8b9dd5c5d68
Robert Yang <priest321@hotmail.com>
2024-05-02 14:54:24 +1000

    Refactor llm_test with new file name

--- model/llm_test.py
+++ model/llm_test.py
@@ -1,14 +1,15 @@
 import torch
-import LLM_own_embedding
+import llm_model
 
-from LLM_own_embedding import AttentionModule, FeedforwardNetwork, MultiHeadModule, TransformerBlock, LLMModel
+from llm_model import AttentionModule, FeedforwardNetwork, MultiHeadModule, TransformerBlock, LLMModel
 
 
-DEVICE = LLM_own_embedding.DEVICE
-model = torch.load("LLM.pth")
+DEVICE = llm_model.DEVICE
+model = torch.load("../pretrained_model/LLM.pth")
 model.eval()
 
-start = model.word_to_index['in']LLMModel
+start = model.word_to_index['in']
 x = torch.tensor([[start]], dtype=torch.long, device=DEVICE)
 print(model.generate(x))
+
 
